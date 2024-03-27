b36e79cd04dbcd3a7cf402225dac4774a66eed72
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-03-27 01:45:10 +0100

    squareroot

--- EloquentJavaScript/sqaureroot.js
+++ EloquentJavaScript/sqaureroot.js
@@ -1,3 +1,17 @@
-let number = prompt("Pick a number");
+const readline = require('readline');
 
-console.log(number);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Pick a number: ', (number) => {
+    if (!Number.isNaN(number)){
+        console.log('"Your number is the square root of ":', number * number);
+    }
+    else{
+        console.log("You didn't enter a number");
+    }
+    rl.close();
+});
+
