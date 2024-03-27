ca847e84a351cd78ca7f968b3cc67451b568e9c4
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-03-27 01:44:11 +0100

    while loop practise

---
+++ EloquentJavaScript/loop.js
@@ -0,0 +1,11 @@
+let number = 1;
+
+while(number <= 12){
+    let count = 1;
+    while (count <= 12){
+        console.log(`${number} X ${count} = ${number * count}`);
+        count++;
+    }
+    console.log("");
+    number++;
+}
