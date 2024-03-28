7e7a8b51c096bdb1e35a129b3e6c4b82c2a32941
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-03-28 11:06:21 +0100

    Javascript task one

---
+++ EloquentJavaScript/task1.js
@@ -0,0 +1,27 @@
+let x = 8;
+let y = 5;
+
+console.log(`Number 1: ${(x + y) + (x + y)}`);
+
+let value = 34;
+remainder = 34 % 10;
+console.log(`Number 2: ${value + remainder}`);
+
+let a = 2;
+let n = 5;
+console.log(`Number 3: ${a ** 5}`);
+
+function task4(){
+    let a = 50
+    let x = 10 + a
+    let y = a - 10
+    let z = a % 10
+    let result = a / (x * y * z)
+
+    return result;
+} 
+
+console.log(`Number 4: ${task4()}`);
+
+let a1 = 20
+console.log(typeof(null))
