9c72e3e8914bdbdeecaa089c0dc5c2c32147f661
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-04-16 15:09:27 +0100

    max

---
+++ EloquentJavaScript/chapterFour/max.js
@@ -0,0 +1,12 @@
+const max = (...numbers)=> {
+    let result = -Infinity;
+    for (let number of numbers){
+        if (number > result) {
+            result = number;
+        }
+    }
+    return result;
+}
+
+console.log(max(2,90,8,23,3));
+console.log("hi", "world");
