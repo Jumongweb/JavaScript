a33a8716342ea80969258225f5e28e7d0a094684
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-04-16 05:00:56 +0100

    is even

---
+++ EloquentJavaScript/chapterThree/exercise/isEven.js
@@ -0,0 +1,15 @@
+const isEven = (number)=>{
+    if (number === 0){
+        return true;
+    }
+    else if (number === 1){
+        return false;
+    }
+    else if(number < 1){
+        return isEven(-number);
+    }
+    else{
+        return isEven(number - 2);
+    }
+}
+console.log(isEven(-70));
